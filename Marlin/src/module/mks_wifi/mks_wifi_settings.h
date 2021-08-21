1964f9ab59ee7bb9254b3fbfa434492312aa49dd
Sergey Turnaev <turnaev@inbox.ru>
2021-08-21 18:10:47 +0300

    Добавлены настройки WiFi

--- Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++ Marlin/src/module/mks_wifi/mks_wifi_settings.h
@@ -1,9 +1,9 @@
 #ifndef MKS_WIFI_SETTINGS_H
 #define MKS_WIFI_SETTINGS_H
 
-//#define MKS_WIFI_ENABLED_WIFI_CONFIG 
+#define MKS_WIFI_ENABLED_WIFI_CONFIG 
 
-#define MKS_WIFI_SSID					"network"
-#define MKS_WIFI_KEY					"password"
+#define MKS_WIFI_SSID					"TSS_WiFi_2.4GHz"
+#define MKS_WIFI_KEY					"X52%4858REFG"
 
 #endif
