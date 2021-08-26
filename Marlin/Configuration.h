a5f7bcab5278810ab40ca394b6145ded92646cb8
Sergey Turnaev <turnaev@inbox.ru>
2021-08-26 21:16:03 +0300

    Поменял шаги и ускорения при загрузке филамента

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -916,7 +916,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 405, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
